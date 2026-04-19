5d85b4af720b4008c082c94c4fd2d181006851b5
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2026-04-19 08:26:06 +0530

    delete: unused components and legacy dependencies
    Signed-off-by: M-DEV-1 <mahadevankizhakkedathu@gmail.com>

--- voxpilot-adk/src/utils/api.ts
+++
@@ -1,8 +0,0 @@
-export const validateApiKey = async (key: string) => {
-	try {
-		const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${key}`);
-		return response.ok;
-	} catch (e) {
-		return false;
-	}
-};
